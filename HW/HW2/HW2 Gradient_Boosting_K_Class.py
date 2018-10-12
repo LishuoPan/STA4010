dfbd4075d66f7912aa6807b2f5f2a58a8f60f709
pls19961219@gmail.com <pls19961219@gmail.com>
2018-10-12 16:57:23 +0800

    HW2 Add remarks

--- HW/HW2/HW2 Gradient_Boosting_K_Class.py
+++ HW/HW2/HW2 Gradient_Boosting_K_Class.py
@@ -4,9 +4,8 @@
 # no fancy packages.
 import numpy as np
 from sklearn import tree
-
-# In[11]:
-
+# this package is used to plot the result
+import matplotlib.pyplot as plt
 
 # Here I define a class as an agent to do
 # Gradient Boosting for K-Class Classification
@@ -34,7 +33,7 @@ class K_GBoost():
         return f
     
     @staticmethod
-    # this method is aimed to 
+    # this method is aimed to compute the P matrix
     def p(k,f):
         # f.dtype = "float64"
         # print(f)
@@ -52,6 +51,8 @@ class K_GBoost():
         z[rev] = 0
         return z
     @staticmethod
+    # this method is try to compute the current f:
+    # i.e. f_km
     def cur_f(reg_tree, gamma, X, reg_room):
         pred = reg_tree.predict(X)
         f_val = np.zeros(X.shape[0])
@@ -61,7 +62,8 @@ class K_GBoost():
         return f_val
 
     def fit(self):
-        # initial the F and P matrix, both are n y k
+        ## Here preallocate some 2-D list or Matrix we will use later
+        # initial the F and P matrix, both are n by k
         F = list()
         # k_f_set is a collection of classifier for the k class
         # add the init classifier to the f_set, return 0 column
@@ -69,14 +71,14 @@ class K_GBoost():
             k_f_set = [lambda X,k,m: np.zeros(X.shape[0])] * self.Max_iter
             F.append(k_f_set)
         assert len(F) == self.K and len(F[0]) == self.Max_iter
-        # print(F)
+
         P = np.zeros((self.X.shape[0], self.K))
-        # print(P)
+        # these is to store the tree, gamma, rooms
         reg_tree_set = [[0]*self.Max_iter for i in range(self.K)]
         gamma_set = [[0] * self.Max_iter for i in range(self.K)]
         reg_rooms_set = [[0] * self.Max_iter for i in range(self.K)]
 
-
+        # Set the P_k matrix in every iteration
         for m in range(self.Max_iter):
 
             f = np.zeros((self.X.shape[0], self.K))
@@ -85,7 +87,7 @@ class K_GBoost():
             for k in range(self.K):
                 P = self.p(k, f)
 
-
+            ## update f
             for k in range(self.K):
                 # k_f_set = F[k]
                 # f = self.f_val(k_f_set, self.X)
@@ -97,7 +99,8 @@ class K_GBoost():
                 y_k = self.y_indicator(self.y, k, self.element)
                 rk = y_k - pk
 
-                reg_tree_set[k][m] = tree.DecisionTreeRegressor()
+                # Use a simple tree in each sub-classifier
+                reg_tree_set[k][m] = tree.DecisionTreeRegressor(max_depth=2)
                 reg_tree_set[k][m] = reg_tree_set[k][m].fit(self.X, rk)
 
                 # all prediction value
@@ -107,7 +110,7 @@ class K_GBoost():
                 reg_rooms_set[k][m] = np.unique(reg_X_train)
                 # preallocation the gamma vector
                 gamma_set[k][m] = np.zeros(len(reg_rooms_set[k][m]))
-
+                # compute the gamma_jkm
                 for j, room_num in enumerate(reg_rooms_set[k][m]):
                     cur_cluster = reg_rooms_set[k][m][j]
                     # index of X_train fell into the j-th room
@@ -115,9 +118,12 @@ class K_GBoost():
                     nu = sum(rk[index_X_in])
                     de = sum(np.abs(rk[index_X_in])*(1-np.abs(rk[index_X_in])))
                     gamma_set[k][m][j] = ((self.K - 1) / self.K) * (nu/de)
-
+                # store the sub-f_km in the F matrix
                 F[k][m]=lambda X,k,m: self.cur_f(reg_tree_set[k][m], gamma_set[k][m], X, reg_rooms_set[k][m])
+        # Store the F matrix as global in the class
         self.f_model = F
+
+    ## this method is to predict the f(X) given X matrix
     def predict(self,X):
         pred = np.zeros((X.shape[0],self.K))
         for k in range(self.K):
@@ -173,13 +179,35 @@ if __name__ == '__main__':
     element = -np.unique(train_y)
     K = len(element)
 
+    # Make two List: iteration list and Acu list.
+    # Used to plot
+    M = np.arange(21)
+    Acu = np.zeros(len(M))
 
+    for trial,iter in enumerate(M):
+        GBoost = K_GBoost(train_x,train_y,iter,element)
+        model = GBoost.fit()
+        pred = GBoost.predict(test_x)
+        # print(pred)
+        pred_class = GBoost.p(K,pred)
+        # print(pred_class)
+        # Accuracy
+        pred_final = np.zeros((test_y.shape[0]))
+        for i,val in enumerate(pred_class):
+            if val[0]>val[1]:
+                pred_final[i] = 1
+            else:
+                pred_final[i] = -1
+        Acu[trial] = sum(pred_final==test_y)/len(test_y)
+    # print the iteration and corresponding Acu
+    print(M)
+    print(Acu)
+    # plot the
+    plt.plot(M,Acu)
+    plt.xlabel("the iteration number")
+    plt.ylabel("the Accuracy")
+    plt.show()
 
-    GBoost = K_GBoost(train_x,train_y,50,element)
-    model = GBoost.fit()
-    pred = GBoost.predict(test_x)
-    print(pred)
-# In[12]:
 
 
 
