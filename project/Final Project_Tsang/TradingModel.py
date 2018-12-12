1ef5ca3beafdc58b93c7fad6fec8cd7f5f8ff30e
pls19961219@gmail.com <pls19961219@gmail.com>
2018-12-12 15:26:39 +0800

    Data input

---
+++ project/Final Project_Tsang/TradingModel.py
@@ -0,0 +1,13 @@
+# utf - 8
+# Author: Lishuo Pan Data: Dec 12 2018
+# load packages
+# import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# construct packages
+class TradingModel:
+    def __init__(self):
+
+
