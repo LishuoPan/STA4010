533d5415e2629be0c44532a88c89ef06eb45c319
pls19961219@gmail.com <pls19961219@gmail.com>
2018-12-13 20:32:41 +0800

    profit

--- project/Final Project_Tsang/TradingModel.py
+++ project/Final Project_Tsang/TradingModel.py
@@ -10,6 +10,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.utils import resample
 
 # import GPy
 # construct packages
@@ -20,10 +22,13 @@ class TradingModel:
         self.X_tr = X_tr
         self.money = money
         self.train_size = 200
-        self.pred_size = 10
-        self.split = 20
+        self.pred_size = 20
+        self.split = 30
         self.stock = list()
         self.short_sell = list()
+        self.inventory_Max = self.split
+        self.hold_max = 500
+        self.resampling = 2
         print("Money at the Begining of the day:", self.money)
 
     ##########################################################
@@ -55,12 +60,15 @@ class TradingModel:
 
         return model_bid, model_ask
     def AdaBoostReg_fit(self, X_tr, y_tr):
-        Ada_reg_model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
-                                            n_estimators=500)
+        Ada_reg_model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=2),
+                                            n_estimators=300,random_state=0,loss='linear')
         Ada_reg_model.fit(X_tr, y_tr)
         return Ada_reg_model
-
-
+    def GradientBoosting(self, X_tr, y_tr):
+        GB_model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1,
+                                             max_depth = 1, random_state = 0, loss = 'ls')
+        GB_model.fit(X_tr, y_tr)
+        return GB_model
     ##########################################################
     # Model behavior Part
     ##########################################################
@@ -69,25 +77,37 @@ class TradingModel:
         Low = np.min(pred)
         return High, Low
     def buy_stock(self,Ask_t, inven):
-        price = Ask_t
-        share = inven/Ask_t
-        self.stock.append([price, share])
-        self.money -= inven
+        if len(self.stock) < self.inventory_Max:
+            price = Ask_t
+            share = inven/Ask_t
+            hold_time = 0
+            self.stock.append([price, share, hold_time])
+            self.money -= inven
+        else:
+            pass
 
     def buy_short_sell(self,Bid_t, inven):
-        price = Bid_t
-        share = inven/Bid_t
-        self.short_sell.append([price, share])
-        self.money += inven
-
+        if len(self.short_sell) < self.inventory_Max:
+            price = Bid_t
+            share = inven/Bid_t
+            hold_time = 0
+            self.short_sell.append([price, share, hold_time])
+            self.money += inven
+        else:
+            pass
 
     def sell_stock(self,y_bid):
         for index, eval in enumerate(self.stock):
             price = eval[0]
             share = eval[1]
+            eval[2] += 1
             earn = y_bid - price
             # exercise once have profit
             if earn > 0:
+                self.money += y_bid * share
+                self.stock.pop(index)
+
+            elif eval[2] > self.hold_max:
                 self.money += y_bid * share
                 self.stock.pop(index)
 
@@ -95,10 +115,15 @@ class TradingModel:
         for index, eval in enumerate(self.short_sell):
             price = eval[0]
             share = eval[1]
+            eval[2] += 1
             earn = price - y_ask
             # exercise once have profit
             if earn > 0:
                 self.money -= y_ask*share
+                self.short_sell.pop(index)
+
+            elif eval[2] > self.hold_max:
+                self.money -= y_ask * share
                 self.short_sell.pop(index)
 
     def Train_Matrix_t(self,t,X_te,y_bid,y_ask):
@@ -155,9 +180,10 @@ class TradingModel:
         tol_t = len(X_te)
         # For each processing time
         for t in range(tol_t):
+            [Bid_t, Ask_t] = [y_te_bid[t],y_te_ask[t]]
             # At the end of the day
             if t == tol_t - 1:
-                self.settle_accounts(y_te_bid[t],y_te_ask[t])
+                self.settle_accounts(Bid_t,Ask_t)
                 print("At the end of the day:", self.money)
 
 
@@ -169,28 +195,53 @@ class TradingModel:
                 [X_tr_t, y_tr_bid, y_tr_ask] = \
                     self.Train_Matrix_t(t,X_te,y_te_bid,y_te_ask)
 
-                # Fit AdaBoost regression model
-                AdaReg_bid_fit = self.AdaBoostReg_fit(X_tr_t, y_tr_bid)
-                AdaReg_ask_fit = self.AdaBoostReg_fit(X_tr_t, y_tr_ask)
+                # resmapling
+                sum_pred_bid = 0
+                sum_pred_ask = 0
+                for re in range(self.resampling):
+                    [X_tr_t_B, y_tr_bid_B, y_tr_ask_B] = resample(X_tr_t, y_tr_bid, y_tr_ask)
 
-                # construct the foresee test data
-                [X_pred,index_te] = self.Test_Matrix_t(t,X_te)
-                # predict the future time of pred_size
-                y_bid_pred = AdaReg_bid_fit.predict(X_pred)
-                y_ask_pred = AdaReg_ask_fit.predict(X_pred)
 
+                    # Fit AdaBoost regression model
+                    AdaReg_bid_fit = self.AdaBoostReg_fit(X_tr_t_B, y_tr_bid_B)
+                    AdaReg_ask_fit = self.AdaBoostReg_fit(X_tr_t_B, y_tr_ask_B)
+
+                    # AdaReg_bid_fit = self.GradientBoosting(X_tr_t, y_tr_bid)
+                    # AdaReg_ask_fit = self.GradientBoosting(X_tr_t, y_tr_ask)
+
+                    # [AdaReg_bid_fit, AdaReg_ask_fit] = \
+                    #     self.WLS_regression(X_tr_t, y_tr_bid, y_tr_ask,count_down=1, p = 2)
+
+
+                    # construct the foresee test data
+                    [X_pred,index_te] = self.Test_Matrix_t(t,X_te)
+                    # predict the future time of pred_size
+                    y_bid_pred_B = AdaReg_bid_fit.predict(X_pred)
+                    y_ask_pred_B = AdaReg_ask_fit.predict(X_pred)
+                    sum_pred_bid += y_bid_pred_B
+                    sum_pred_ask += y_ask_pred_B
+
+                y_bid_pred = sum_pred_bid/self.resampling
+                y_ask_pred = sum_pred_ask/self.resampling
                 # After the prediction,
                 # the process model will decide
                 # wether buy in stock or short sell
                 [Bid_pred_max, Bid_pred_min] = self.FindHL(y_bid_pred)
                 [Ask_pred_max, Ask_pred_min] = self.FindHL(y_ask_pred)
-                [Bid_t, Ask_t] = [y_te_bid[t],y_te_ask[t]]
-                Buy_stock_indicator = Bid_pred_max > Ask_t
+                Buy_stock_indicator = Bid_pred_max*0.999 > Ask_t
                 Buy_short_sell_indicator = Ask_pred_min < Bid_t
 
-                # plt.plot(y_bid_pred)
-                # plt.plot(y_te_bid[index_te])
-                # plt.show()
+
+                # f = plt.figure()
+                # ax = f.add_subplot(111)
+                # ax.plot(y_bid_pred,label = 'bidp')
+                # ax.plot(y_ask_pred,label = 'askp')
+                # ax.plot(y_te_bid[index_te],label = 'bid')
+                # ax.plot(y_te_ask[index_te],label = 'askp')
+                # ax.legend()
+                # f.show()
+                # plt.close(f)
+
                 # Money you willing to pay at this round
                 if self.money>0:
                     inven = self.money/self.split
@@ -203,8 +254,12 @@ class TradingModel:
 
 
                 # run though the current inventory to see if there is profit
-                self.sell_stock(y_te_bid[t])
-                self.promise_short_sell(y_te_ask[t])
+                # sell_stock_indicator = Bid_pred_max < Bid_t
+                # sell_short_sell_indicator = Ask_pred_min > Ask_t
+                # if sell_stock_indicator == True:
+                self.sell_stock(Bid_t)
+                # if sell_short_sell_indicator == True:
+                self.promise_short_sell(Ask_t)
 
 
                 print("At time:", t,"you have money:",self.money,
